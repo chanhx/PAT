f7d52537798fdccd42223e25bc7c551fe3976b06
chanhx <ichan.hocheung@gmail.com>
2018-06-30 19:27:04 +0800

    1103. Integer Factorization (30)

---
+++ 1103/1103.cc
@@ -0,0 +1,88 @@
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void FindFactors(int target, int k, int rk, 
+                 int factors_sum, int &pre_factors_sum,
+                 vector<int> &largest_seq,
+                 vector<int> &factors,
+                 vector<int> &ppow)
+{
+    if (target < k - rk) {return;}
+
+    int upper_bound = rk > 1 ? factors[rk-2] : ppow.size()-1;
+    for (int h = upper_bound, l = 1, limit = target-(k-rk); h > l;) {
+        int upper_bound = (h + l) / 2;
+        if (ppow[upper_bound] > limit) {
+            h = upper_bound - 1;
+        } else {
+            break;
+        }
+    }
+
+    if (pre_factors_sum > factors_sum) {
+        int diff = pre_factors_sum - factors_sum;
+        if (diff > upper_bound * (k-rk+1)) {
+            return;
+        }
+    }
+
+    for (int i = upper_bound; i >= 1; --i) {
+
+        int new_target = target - ppow[i];
+
+        if (rk < k) {
+            factors[rk-1] = i;
+            FindFactors(new_target, k, rk+1, 
+                        factors_sum + i, pre_factors_sum, 
+                        largest_seq, factors, ppow);
+        } else if (rk == k && new_target == 0) {
+            factors_sum += i;
+            if (factors_sum > pre_factors_sum) {
+                factors[rk-1] = i;
+                pre_factors_sum = factors_sum;
+                largest_seq = factors;
+            }
+            return;
+        } else if (rk == k && new_target > 0) {
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int n, k, p;
+    scanf("%d %d %d", &n, &k, &p);
+
+    vector<int> ppow;
+    ppow.push_back(0);
+
+    for (int i = 1, upper_bound = n-k+1; i <= upper_bound; ++i) {
+        int ippow = pow(i, p);
+        if (ippow > upper_bound) {break;}
+
+        ppow.push_back(ippow);
+    }
+
+    vector<int> largest_seq(k), factors(k);
+    int pre_factors_sum = 0;
+    FindFactors(n, k, 1, 0, pre_factors_sum, largest_seq, factors, ppow);
+
+    if (largest_seq[0] == 0) {
+        printf("Impossible\n");
+        return 0;
+    }
+
+    printf("%d = ", n);
+    for (int i = 0; i < k; ++i) {
+        if (i != 0) {printf(" + ");}
+        printf("%d^%d", largest_seq[i], int(p));
+    }
+    printf("\n");
+    
+    return 0;
+}
--- README.md
+++ README.md
@@ -25,3 +25,4 @@
 | 1085 | [Perfect Sequence (25)](1085)|
 | 1092 | [To Buy or Not to Buy (20)](1092)|
 | 1093 | [Count PAT's (25)](1093)|
+| 1103 | [Integer Factorization (30)](1103)|
