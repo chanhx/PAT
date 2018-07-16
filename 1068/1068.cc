4262ecb163ef0db115184b50f7fe0c709a81cd21
chanhx <ichan.hocheung@gmail.com>
2018-07-16 23:37:37 +0800

    1068. Find More Coins (30)
    DFS

--- 1068/1068.cc
+++ 1068/1068.cc
@@ -1,44 +1,53 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
+
+bool dfs(int i, int m, vector<int> &coins, vector<int> &left_sum, vector<int> &ans)
+{
+    if (m == 0) {
+        return true;
+    }
+
+    if (m < 0 || i >= coins.size() || m > left_sum[i]) {
+        return false;
+    }
+
+    if (dfs(i+1, m-coins[i], coins, left_sum, ans)) {
+        ans.push_back(coins[i]);
+        return true;
+    } else if (dfs(i+1, m, coins, left_sum, ans)) {
+        return true;
+    }
+
+    return false;
+}
 
 int main()
 {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    vector<int> coins(n, 0);
+    vector<int> coins(n, 0), left_sum(n, 0);
+    
     for (auto &c : coins) {
         scanf("%d", &c);
     }
-    sort(coins.begin(), coins.end(), greater<int>());
+    sort(coins.begin(), coins.end());
 
-    vector<int> dp(m+1, 0);
-    vector<vector<bool>> selected(n, vector<bool>(m+1, false));
-
-    for (int i = 0; i < n; ++i) {
-        for (int v = m; v >= coins[i]; --v) {
-            if (dp[v-coins[i]] + coins[i] < dp[v]) {
-                continue;
-            }
-            dp[v] = dp[v-coins[i]] + coins[i];
-            selected[i][v] = true;
-        }
+    for (int i = n-1, sum = 0; i >= 0; --i) {
+        sum += coins[i];
+        left_sum[i] = sum;
     }
 
-    if (dp[m] != m) {
+    vector<int> ans;
+    if (dfs(0, m, coins, left_sum, ans)) {
+        for (int i = ans.size()-1; i >= 0; --i) {
+            printf("%d%c", ans[i], i == 0 ? '\n' : ' ');
+        }
+    } else {
         printf("No Solution\n");
-        return 0;
-    }
-
-    for (int i = n-1, v = m; i >= 0; --i) {
-        if (selected[i][v]) {
-            v -= coins[i];
-            printf("%d%c", coins[i], v == 0 ? '\n' : ' ');
-        }
     }
 
     return 0;
